5e5e4be26a98a830b0c1764ea00242108566cea3
Jose Labra <jelabra@gmail.com>
2022-05-02 08:47:02 +0200

    Added projectMatrix

--- modules/srdf/src/main/scala/es/weso/rdf/locations/Location.scala
+++ modules/srdf/src/main/scala/es/weso/rdf/locations/Location.scala
@@ -1,12 +1,7 @@
 package es.weso.rdf.locations
 import es.weso.rdf.nodes._
-import es.weso.rdf.triples._
-import io.circe.generic.auto._, io.circe.syntax._
 
 case class Location(line: Long, col: Long, tokenType: String, source: Option[IRI] = None) {
-  override def toString = s"${tokenType}@${line},${col}${source.map(x => s"<-${x.str}").getOrElse("")}"  
+  override def toString =
+    s"${tokenType}@${line},${col}${source.map(x => s"<-${x.str}").getOrElse("")}"
 }
-
-/* object Location {
-  implicit val locationEncoder: Encoder[Location] = implicitly[Encoder[Location]]
-} */
