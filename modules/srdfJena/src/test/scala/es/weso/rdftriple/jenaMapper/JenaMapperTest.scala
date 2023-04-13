a18e599ad3c0bd68474d94c5becb71696faefead
Jose Labra <jelabra@gmail.com>
2023-04-13 22:37:50 +0200

    Added tests in JenaMapperTest...starting to check issue 292

---
+++ modules/srdfJena/src/test/scala/es/weso/rdftriple/jenaMapper/JenaMapperTest.scala
@@ -0,0 +1,162 @@
+package es.weso.rdftriple.jenaMapper
+import org.apache.jena.rdf.model.ModelFactory
+import es.weso.rdf.nodes._
+import es.weso.rdf.jena.JenaMapper._
+import es.weso.rdf.jena._
+import es.weso.rdf.triples.RDFTriple
+import munit._
+import cats._
+import cats.implicits._
+
+class JenaMapperTest extends CatsEffectSuite with JenaBased {
+
+  test("Should compare one triple with 2 different bNodes") {
+    val ts = Set(RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), BNode("b" + 1)))
+    val s = """[] <http://example.org#p> [] ."""
+    val empty = ModelFactory.createDefaultModel
+    val model1 = RDFTriples2Model(ts, empty, None)
+    val model2 = str2model(s)
+    assertEquals(checkIsomorphic(model1, model2), ().asRight)
+  }
+
+  test("Should compare one triple with a shared bNode") {
+    val ts = Set(RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), BNode("b" + 0)))
+    val s = """_:a <http://example.org#p> _:a ."""
+    val empty = ModelFactory.createDefaultModel
+    val model1 = RDFTriples2Model(ts, empty, None)
+    val model2 = str2model(s)
+    assertEquals(checkIsomorphic(model1, model2), ().asRight)
+  }
+
+  test("Should compare one triple with a prefix decl") {
+    val ts = Set(RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), BNode("b" + 0)))
+    val s = """|@prefix : <http://example.org#> .
+               |_:a :p _:a .""".stripMargin
+    val empty = ModelFactory.createDefaultModel
+    val model1 = RDFTriples2Model(ts, empty, None)
+    val model2 = str2model(s)
+    assertEquals(checkIsomorphic(model1, model2), ().asRight)
+  }
+
+  test("Should compare one triple with an integer literal") {
+    val ts = Set(RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), IntegerLiteral(1, "1")))
+    val s = """|@prefix : <http://example.org#> .
+               |_:a :p 1 .""".stripMargin
+    val empty = ModelFactory.createDefaultModel
+    val model1 = RDFTriples2Model(ts, empty, None)
+    val model2 = str2model(s)
+    assertEquals(checkIsomorphic(model1, model2), ().asRight)
+  }
+
+  test("Should compare one triple with a decimal literal") {
+    val ts = Set(RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), DecimalLiteral(1.2)))
+    val s = """|@prefix : <http://example.org#> .
+               |_:a :p 1.2 .""".stripMargin
+    val empty = ModelFactory.createDefaultModel
+    val model1 = RDFTriples2Model(ts, empty, None)
+    val model2 = str2model(s)
+    assertEquals(checkIsomorphic(model1, model2), ().asRight)
+  }
+
+  test("Should compare one triple with a boolean literal") {
+    val ts = Set(RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), BooleanLiteral(true)))
+    val s = """|@prefix : <http://example.org#> .
+               |_:a :p true .""".stripMargin
+    val empty = ModelFactory.createDefaultModel
+    val model1 = RDFTriples2Model(ts, empty, None)
+    val model2 = str2model(s)
+    assertEquals(checkIsomorphic(model1, model2), ().asRight)
+  }
+
+  // The following test fails probably for Double comparison
+  test("Should compare one triple with a double literal".ignore) {
+    val ts = Set(RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), DoubleLiteral(1.2e3)))
+    val s = """|@prefix : <http://example.org#> .
+               |_:a :p 1.2e3 .""".stripMargin
+    val empty = ModelFactory.createDefaultModel
+    val model1 = RDFTriples2Model(ts, empty, None)
+    val model2 = str2model(s)
+    assertEquals(checkIsomorphic(model1, model2), ().asRight)
+  }
+
+  test("Should convert three triples") {
+    val ts = Set(
+      RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), BNode("b" + 0)),
+      RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), IntegerLiteral(4, "4")),
+      RDFTriple(BNode("b" + 0), IRI("http://example.org#p"), LangLiteral("pepe", Lang("es")))
+    )
+    val empty = ModelFactory.createDefaultModel
+    val m1 = RDFTriples2Model(ts, empty, None)
+    val m2 = str2model("""|@prefix : <http://example.org#> .
+                          |_:0 <http://example.org#p> _:0, 4, "pepe"@es .
+                          |""".stripMargin)
+    assertEquals(checkIsomorphic(m1, m2), ().asRight)
+  }
+
+  test("Should check integer ok") {
+    val node = IntegerLiteral(23, "23")
+    wellTypedDatatype(node, IRI("http://www.w3.org/2001/XMLSchema#integer"))
+      .map(assertEquals(_, true))
+  }
+
+  test("Should check string ok") {
+    val node = StringLiteral("Pepe")
+    wellTypedDatatype(node, IRI("http://www.w3.org/2001/XMLSchema#string"))
+      .map(assertEquals(_, true))
+  }
+
+  test("Should check lang string ok") {
+    val node = LangLiteral("Pepe", Lang("es"))
+    wellTypedDatatype(node, IRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#langString"))
+      .map(assertEquals(_, true))
+  }
+
+  test("Should check date ok") {
+    val node = DatatypeLiteral("1981-07-03", IRI("http://www.w3.org/2001/XMLSchema#date"))
+    wellTypedDatatype(node, IRI("http://www.w3.org/2001/XMLSchema#date"))
+      .map(assertEquals(_, true))
+  }
+
+  test("Should check bad string") {
+    val node = DatatypeLiteral("john", IRI("http://www.w3.org/2001/XMLSchema#string"))
+    wellTypedDatatype(node, IRI("http://www.w3.org/2001/XMLSchema#date"))
+      .map(assertEquals(_, false))
+  }
+
+  test("Should check bad date") {
+    intercept[org.apache.jena.datatypes.DatatypeFormatException] {
+      val node = DatatypeLiteral("john", IRI("http://www.w3.org/2001/XMLSchema#date"))
+      wellTypedDatatype(node, IRI("http://www.w3.org/2001/XMLSchema#date")).unsafeRunSync()
+    }
+  }
+
+  test("Should fail checking single string with dateTime") {
+    val node = StringLiteral("not_a_date")
+    wellTypedDatatype(node, IRI("http://www.w3.org/2001/XMLSchema#dateTime"))
+      .map(assertEquals(_, false))
+  }
+
+  test("Should fail checking date with dateTime") {
+    val node = DatatypeLiteral("2017-05-15", IRI("http://www.w3.org/2001/XMLSchema#date"))
+    wellTypedDatatype(node, IRI("http://www.w3.org/2001/XMLSchema#dateTime"))
+      .map(assertEquals(_, false))
+  }
+
+  // Test added at issue: https://github.com/weso/srdf/issues/292
+  test("Should compare one triple with a rdf:HTML literal".ignore) {
+    val ts = Set(
+      RDFTriple(
+        BNode("b" + 0),
+        IRI("http://example.org#p"),
+        RDFHTMLLiteral("<div>Test HTML markup</div>")))
+    val s =
+      """|@prefix : <http://example.org#> .
+         |@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+         |_:a :p "<div>Test HTML markup</div>"^^rdf:HTML .""".stripMargin
+    println(s"ts: $ts")
+    val empty = ModelFactory.createDefaultModel
+    val model1 = RDFTriples2Model(ts, empty, None)
+    val model2 = str2model(s)
+    assertEquals(checkIsomorphic(model1, model2), ().asRight)
+  }
+}
